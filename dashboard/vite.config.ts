1795deb844c3365a208d32623103348e92194acc
avinashp25 <134071895+avinashp25@users.noreply.github.com>
2024-08-04 00:49:05 +0530

    Vite install

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,17 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', // Change output directory from 'dist' to 'build'
-    sourcemap: true, // Enable source maps
-    rollupOptions: {
-      // Customize the output
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'], // Split vendor code
-        },
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
