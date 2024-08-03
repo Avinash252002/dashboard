b99fc4948986af439f9222782d488e40a5f1f349
avinashp25 <134071895+avinashp25@users.noreply.github.com>
2024-08-04 00:27:49 +0530

    Activity header done

--- dashboard/src/Dashboard/AnalyticsGraph.tsx
+++ dashboard/src/Dashboard/AnalyticsGraph.tsx
@@ -1,22 +1,77 @@
-import React from 'react'
-import {Box } from '@mui/material'
+import React from "react";
+import { Box, FormControl, Select, MenuItem, Typography } from "@mui/material";
 
 const AnalyticsGraph = () => {
   return (
     <Box
-    display={"flex"}
-    flexDirection={"row"}
-    gap={2}
-    width={"62.7%"}
-    bgcolor={"#1f2029"}
-    p={2}
-    borderRadius={"8px"}
-    justifyContent={"space-between"}>
-        Analytics 
+      display={"flex"}
+      flexDirection={"column"}
+      gap={2}
+      width={"62.7%"}
+      bgcolor={"#1f2029"}
+      p={2}
+      borderRadius={"8px"}
+    >
+      <Box
+        display={"flex"}
+        flexDirection={"row"}
+        gap={2}
+        justifyContent={"space-between"}
+      >
+        <h3 style={{ fontWeight: "500" }}>Activity</h3>
+        <FormControl sx={{ m: 1, minWidth: 100 }} size="small">
+          <Select
+            size="small"
+            label="Age"
+            sx={{
+              height: "30px",
+              width: "100px",
+
+              fontSize: "12px",
+              fontFamily: "poppins",
+              borderRadius: "20px", // Rounded borders
+              backgroundColor: "#43464b", // Background color red
+              color: "#fff", // Text color white
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "transparent", // Make border color transparent
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                borderColor: "transparent", // Keep border color transparent on hover
+              },
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "transparent", // Keep border color transparent on focus
+              },
+              "& .MuiSelect-icon": {
+                color: "#fff", // Icon color white
+              },
+              "& .MuiSvgIcon-root": {
+                color: "#fff",
+              },
+            }}
+          >
+            <MenuItem
+              value={10}
+              sx={{ fontSize: "12px", fontFamily: "poppins" }}
+            >
+              Weekly
+            </MenuItem>
+            <MenuItem
+              value={20}
+              sx={{ fontSize: "12px", fontFamily: "poppins" }}
+            >
+              Monthly
+            </MenuItem>
+            <MenuItem
+              value={30}
+              sx={{ fontSize: "12px", fontFamily: "poppins" }}
+            >
+              Annualy
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
     </Box>
-      
+  );
+};
 
-  )
-}
-
-export default AnalyticsGraph
+export default AnalyticsGraph;
